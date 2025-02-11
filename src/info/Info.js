8426138646631df7462ce7d60e4f3860e861bc8b
Soumil Singh <singhsoumil805@gmail.com>
2025-02-11 16:32:05 +0530

    info
    File upload

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,92 +1,67 @@
 import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import OpenD1 from "../img/OpenD/Img1.png"
+import OpenD2 from "../img/OpenD/Img2.png"
+import OpenD3 from "../img/OpenD/Img3.png"
+import OpenD4 from "../img/OpenD/Img4.png"
+import OpenD5 from "../img/OpenD/Img5.png"
+import Dsurv1 from "../img/Dsurv/Img1.png"
+import Dsurv2 from "../img/Dsurv/Img2.png"
+import swole from "../img/SwoleNormous.png"
+import SimonGame from "../img/Simon Game.png"
+import Notes from "../img/Notes Keeper.png"
+import Blog1 from "../img/BlogApi/Img1.png"
+import Blog2 from "../img/BlogApi/Img2.png"
+import Blog3 from "../img/BlogApi/Img3.png"
+import Blog4 from "../img/BlogApi/Img4.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
+export let colors = [" rgb(66, 66, 67), rgb(194, 192, 196)"];
 
-/* 
-This variable will change the layout of the website from multipage to single, scrollable page
-*/
-export let singlePage = false;
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Soumil",
+    lastName: "Singh",
+    initials: "SS", 
     position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self,
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [ 
         {
             emoji: '☕',
             text: 'fueled by coffee'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
-        },
-        {
-            emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: 'based in India'
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "singhsoumil805@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/Soumil805",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/soumil-singh-0069bb238/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
         {
-            link: "https://twitter.com",
+            link: "https://x.com/singhsoumil805",
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello Im soumil a Bsc Comp Sci Graduate and a Full stack Developer along with some interest in Web3 and entrepreneurship.",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            exposedTo: ['Java', 'Android Studio', 'C#', 'Kotlin', '.Net', 'Data Structures']
         }
     ,
     hobbies: [
@@ -95,50 +70,74 @@ export const info = {
             emoji: '📖'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
-        },
-        {
             label: 'movies',
             emoji: '🎥'
         },
-        {
-            label: 'cooking',
-            emoji: '🌶'
-        }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+        
+
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [ 
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
+            title: "Open-D",
+            additionalImages: [
+                OpenD1,
+                OpenD2,
+                OpenD3,
+                OpenD4,
+                OpenD5
+            ],
+            description:`A Decentralized NFT marketplace that eliminates reliance on centralized servers, enhancing security and resilience against cyber threats, with a on-chain storage using the Internet Computer Blockchain through Motoko Smart Contracts, ensuring transparency and immutability.
+             Also, an intuitive UI with React was made, optimizing user experience and seamless navigation.`,
+            source: "https://github.com/Soumil805/OpenD", 
+            image: OpenD1
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock2
+            title: "Dsurv",
+            additionalImages: [
+                Dsurv1,
+                Dsurv2
+            ],
+            description: `Introduced SoM Token as the primary transaction currency for OpenD the NFT marketplace, enabling newcomers to receive free tokens for trading on OpenD.
+            Users are also able to check their balance and send tokens to other users`,
+            source: "https://github.com/Soumil805/Token",
+            image: Dsurv1
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
+            title: "Swolenormous",
+            description:`A Fitness web app which provides personalized workouts based on specific user needs.
+            Developed using React and Tailwind CSS, ensuring an interactive and responsive design.`,
+            live: "https://swole-fit.netlify.app/",
+            source: "https://github.com/Soumil805/The_Fit.git",
+            image: swole
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
+            title: "Notes Keeper App",
+            live: "https://3m3tg3.csb.app/",
+            description:"A Google Keep-inspired web app using React.js to manage notes efficiently using custom components, state management, and reusable UI elements, improving user interaction.",
+            source: "https://codesandbox.io/p/sandbox/keeper-app-final-3m3tg3",
+            image: Notes
         },
         {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
-        }
+            title: "Blog Api",
+            additionalImages:[
+                Blog1,
+                Blog2,
+                Blog3,
+                Blog4
+            ],
+            description:`A custom made Api for blogs including event handlers for adding, deleting, editing and extracting all the other Blogs
+            The project also includes a Front-end which showcases a way to access and use the Api`,
+            source: "https://github.com/Soumil805/Blog-Api",
+            image: Blog1
+        },
+        {
+            title: "Simon Game",
+            description:`A simple yet intricate Simon game which uses simple JavaScript logic to create the Back-end conditions to run the game.
+            HTML and CSS are used for the frontend of this game`,
+            live: "https://soumil805.github.io/Simon-Game/",
+            source: "https://github.com/Soumil805/Simon-Game",
+            image: SimonGame
+        },
+        
     ]
 }
